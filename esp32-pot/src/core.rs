20b802c221a533bb989d1438894ef413e858bcbe
rybmks <rybalkomaks3@gmail.com>
2025-04-03 16:32:17 +0300

    Esp32 server

---
+++ esp32-pot/src/core.rs
@@ -0,0 +1,6 @@
+mod private {}
+
+crate::mod_interface! {
+    layer error;
+    layer esp;
+}
---
+++ esp32-pot/src/lib.rs
@@ -0,0 +1,7 @@
+use mod_interface::mod_interface;
+
+mod private {}
+
+crate::mod_interface! {
+    layer core;
+}
